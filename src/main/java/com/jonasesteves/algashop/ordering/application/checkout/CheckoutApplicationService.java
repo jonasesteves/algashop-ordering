a29bad3ea35ab748db237687337e0a058c5473c8
Jonas Esteves <jonasestevess@gmail.com>
2026-01-24 13:36:34 +0000

    Implementation of checkout service

--- src/main/java/com/jonasesteves/algashop/ordering/application/checkout/CheckoutApplicationService.java
+++ src/main/java/com/jonasesteves/algashop/ordering/application/checkout/CheckoutApplicationService.java
@@ -1,12 +1,14 @@
 package com.jonasesteves.algashop.ordering.application.checkout;
 
-import com.jonasesteves.algashop.ordering.domain.model.order.PaymentMethod;
+import com.jonasesteves.algashop.ordering.domain.model.commons.ZipCode;
+import com.jonasesteves.algashop.ordering.domain.model.order.*;
 import com.jonasesteves.algashop.ordering.domain.model.order.shipping.OriginAddressService;
 import com.jonasesteves.algashop.ordering.domain.model.order.shipping.ShippingCostService;
 import com.jonasesteves.algashop.ordering.domain.model.shoppingcart.ShoppingCart;
 import com.jonasesteves.algashop.ordering.domain.model.shoppingcart.ShoppingCartId;
 import com.jonasesteves.algashop.ordering.domain.model.shoppingcart.ShoppingCartNotFoundException;
 import com.jonasesteves.algashop.ordering.domain.model.shoppingcart.ShoppingCarts;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -15,19 +17,48 @@ import java.util.Objects;
 public class CheckoutApplicationService {
 
     private final ShoppingCarts shoppingCarts;
+    private final ShippingCostService shippingCostService;
+    private final OriginAddressService originAddressService;
+    private final CheckoutService checkoutService;
+    private final ShippingInputDisassembler shippingInputDisassembler;
+    private final BillingInputDisassembler billingInputDisassembler;
+    private final Orders orders;
 
-    public CheckoutApplicationService(ShoppingCarts shoppingCarts) {
+    public CheckoutApplicationService(ShoppingCarts shoppingCarts, ShippingCostService shippingCostService, OriginAddressService originAddressService, CheckoutService checkoutService, ShippingInputDisassembler shippingInputDisassembler, BillingInputDisassembler billingInputDisassembler, Orders orders) {
         this.shoppingCarts = shoppingCarts;
+        this.shippingCostService = shippingCostService;
+        this.originAddressService = originAddressService;
+        this.checkoutService = checkoutService;
+        this.shippingInputDisassembler = shippingInputDisassembler;
+        this.billingInputDisassembler = billingInputDisassembler;
+        this.orders = orders;
     }
 
-    public void checkout(CheckoutInput checkoutInput) {
+    @Transactional
+    public String checkout(CheckoutInput checkoutInput) {
         Objects.requireNonNull(checkoutInput);
         PaymentMethod paymentMethod = PaymentMethod.valueOf(checkoutInput.getPaymentMethod());
 
         ShoppingCart shoppingCart = shoppingCarts.ofId(new ShoppingCartId(checkoutInput.getShoppingCartId()))
                 .orElseThrow(ShoppingCartNotFoundException::new);
 
-//        ShippingCostService;
-//        OriginAddressService
+        ShippingCostService.CalculationResult shippingCalculationResult = calculateShippingCost(checkoutInput.getShipping());
+
+        Shipping shipping = shippingInputDisassembler.toDomainModel(checkoutInput.getShipping(),  shippingCalculationResult);
+        Billing billing = billingInputDisassembler.toDomainModel(checkoutInput.getBilling());
+
+        Order order = checkoutService.checkout(shoppingCart, billing, shipping, paymentMethod);
+        orders.add(order);
+        shoppingCarts.add(shoppingCart);
+
+        return order.id().toString();
+
+    }
+
+    private ShippingCostService.CalculationResult calculateShippingCost(ShippingInput shipping) {
+        ZipCode origin = originAddressService.originAddress().zipCode();
+        ZipCode destination = new ZipCode(shipping.getAddress().getZipCode());
+
+        return shippingCostService.calculate(new ShippingCostService.CalculationRequest(origin, destination));
     }
 }
---
+++ src/test/java/com/jonasesteves/algashop/ordering/application/checkout/CheckoutApplicationServiceIT.java
@@ -0,0 +1,129 @@
+package com.jonasesteves.algashop.ordering.application.checkout;
+
+import com.jonasesteves.algashop.ordering.domain.model.commons.Money;
+import com.jonasesteves.algashop.ordering.domain.model.commons.Quantity;
+import com.jonasesteves.algashop.ordering.domain.model.customer.CustomerTestDataBuilder;
+import com.jonasesteves.algashop.ordering.domain.model.customer.Customers;
+import com.jonasesteves.algashop.ordering.domain.model.order.Order;
+import com.jonasesteves.algashop.ordering.domain.model.order.OrderId;
+import com.jonasesteves.algashop.ordering.domain.model.order.OrderStatus;
+import com.jonasesteves.algashop.ordering.domain.model.order.Orders;
+import com.jonasesteves.algashop.ordering.domain.model.order.shipping.ShippingCostService;
+import com.jonasesteves.algashop.ordering.domain.model.product.Product;
+import com.jonasesteves.algashop.ordering.domain.model.product.ProductTestDataBuilder;
+import com.jonasesteves.algashop.ordering.domain.model.shoppingcart.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootTest
+@Transactional
+class CheckoutApplicationServiceIT {
+
+    @Autowired
+    private CheckoutApplicationService service;
+
+    @Autowired
+    private Orders orders;
+
+    @Autowired
+    private ShoppingCarts shoppingCarts;
+
+    @Autowired
+    private Customers customers;
+
+    @MockitoBean
+    private ShippingCostService shippingCostService;
+
+    @BeforeEach
+    void setup() {
+        Mockito.when(shippingCostService.calculate(Mockito.any(ShippingCostService.CalculationRequest.class)))
+                .thenReturn(new ShippingCostService.CalculationResult(
+                        new Money("10.00"),
+                        LocalDate.now().plusDays(3)
+                ));
+
+        if (!customers.exists(CustomerTestDataBuilder.DEFAULT_CUSTOMER_ID)) {
+            customers.add(CustomerTestDataBuilder.existingCustomer().build());
+        }
+    }
+
+    @Test
+    void shouldCheckout() {
+        Product product = ProductTestDataBuilder.someProduct().inStock(true).build();
+
+        ShoppingCart shoppingCart = ShoppingCartTestDataBuilder.someEmptyShoppingCart();
+        shoppingCart.addItem(product, new Quantity(1));
+        shoppingCarts.add(shoppingCart);
+
+        CheckoutInput input = CheckoutInputTestDataBuilder.someCheckoutInput()
+                .shoppingCartId(shoppingCart.id().value())
+                .build();
+
+
+        String orderId = service.checkout(input);
+
+        Assertions.assertThat(orderId).isNotBlank();
+        Assertions.assertThat(orders.exists(new OrderId(orderId))).isTrue();
+
+        Optional<Order> createdOrder = orders.ofId(new OrderId(orderId));
+        Assertions.assertThat(createdOrder).isPresent();
+        Assertions.assertThat(createdOrder.get().status()).isEqualTo(OrderStatus.PLACED);
+        Assertions.assertThat(createdOrder.get().totalAmount().value()).isGreaterThan(BigDecimal.ZERO);
+
+        Optional<ShoppingCart> updatedCart = shoppingCarts.ofId(shoppingCart.id());
+        Assertions.assertThat(updatedCart).isPresent();
+        Assertions.assertThat(updatedCart.get().isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldThrowShoppingCartNotFoundExceptionWhenCheckoutWithNonExistingShoppingCart() {
+        CheckoutInput input = CheckoutInputTestDataBuilder.someCheckoutInput()
+                .shoppingCartId(UUID.randomUUID())
+                .build();
+
+        Assertions.assertThatExceptionOfType(ShoppingCartNotFoundException.class)
+                .isThrownBy(() -> service.checkout(input));
+    }
+
+    @Test
+    void shouldThrowShoppingCartCantProceedToCheckoutExceptionWhenCartIsEmpty() {
+        ShoppingCart shoppingCart = ShoppingCartTestDataBuilder.someEmptyShoppingCart();
+        shoppingCarts.add(shoppingCart);
+
+        CheckoutInput input = CheckoutInputTestDataBuilder.someCheckoutInput()
+                .shoppingCartId(shoppingCart.id().value())
+                .build();
+
+        Assertions.assertThatExceptionOfType(ShoppingCartCantProceedToCheckoutException.class)
+                .isThrownBy(() -> service.checkout(input));
+    }
+
+    @Test
+    void shouldThrowShoppingCartCantProceedToCheckoutExceptionWhenCartContainsUnavailableItems() {
+        Product product = ProductTestDataBuilder.someProduct().inStock(true).build();
+        Product unavailableProduct = ProductTestDataBuilder.someProduct().id(product.id()).inStock(false).build();
+
+        ShoppingCart shoppingCart = ShoppingCartTestDataBuilder.someEmptyShoppingCart();
+        shoppingCart.addItem(product, new Quantity(1));
+        shoppingCart.refreshItem(unavailableProduct);
+        shoppingCarts.add(shoppingCart);
+
+        CheckoutInput input = CheckoutInputTestDataBuilder.someCheckoutInput()
+                .shoppingCartId(shoppingCart.id().value())
+                .build();
+
+        Assertions.assertThatExceptionOfType(ShoppingCartCantProceedToCheckoutException.class)
+                .isThrownBy(() -> service.checkout(input));
+    }
+}
