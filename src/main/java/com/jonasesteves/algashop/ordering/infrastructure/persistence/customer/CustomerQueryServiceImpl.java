362a5497f08a2ea0c95afaa90a329c5c08dac9e8
Jonas Esteves <jonasestevess@gmail.com>
2026-05-02 16:21:38 +0100

    Implementing CQRS

--- src/main/java/com/jonasesteves/algashop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
+++ src/main/java/com/jonasesteves/algashop/ordering/infrastructure/persistence/customer/CustomerQueryServiceImpl.java
@@ -2,26 +2,62 @@ package com.jonasesteves.algashop.ordering.infrastructure.persistence.customer;
 
 import com.jonasesteves.algashop.ordering.application.customer.query.CustomerOutput;
 import com.jonasesteves.algashop.ordering.application.customer.query.CustomerQueryService;
-import com.jonasesteves.algashop.ordering.application.utility.Mapper;
 import com.jonasesteves.algashop.ordering.domain.model.customer.CustomerNotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+import org.modelmapper.internal.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
+@Transactional(readOnly = true)
 public class CustomerQueryServiceImpl implements CustomerQueryService {
 
-    private final CustomerPersistenceEntityRepository repository;
-    private final Mapper mapper;
+    private final EntityManager entityManager;
 
-    public CustomerQueryServiceImpl(CustomerPersistenceEntityRepository repository, Mapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
+    private static final String findByIdAsOutputJPQL = """
+            SELECT new com.jonasesteves.algashop.ordering.application.customer.query.CustomerOutput(
+                c.id,
+                c.firstName,
+                c.lastName,
+                c.email,
+                c.phone,
+                c.document,
+                c.birthDate,
+                c.promotionNotificationsAllowed,
+                c.loyaltyPoints,
+                c.registeredAt,
+                c.archivedAt,
+                c.archived,
+                new com.jonasesteves.algashop.ordering.application.commons.AddressData(
+                    c.address.street,
+                    c.address.number,
+                    c.address.city,
+                    c.address.state,
+                    c.address.zipCode,
+                    c.address.complement,
+                    c.address.neighborhood
+                )
+            )
+            FROM CustomerPersistenceEntity c
+            WHERE c.id = :id""";
+
+    public CustomerQueryServiceImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @Override
     public CustomerOutput findById(UUID customerId) {
-        CustomerPersistenceEntity customer = repository.findById(customerId).orElseThrow(CustomerNotFoundException::new);
-        return mapper.convert(customer, CustomerOutput.class);
+        try {
+            TypedQuery<CustomerOutput> query = entityManager.createQuery(findByIdAsOutputJPQL, CustomerOutput.class);
+            query.setParameter("id", customerId);
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            throw new CustomerNotFoundException();
+        }
     }
 }
